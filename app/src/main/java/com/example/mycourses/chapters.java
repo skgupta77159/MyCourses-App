57310b0834da80e25bc90347898e0fde04f6704c
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-01-18 21:31:35 +0530

    Second Commit by Sushil Kumar

--- app/src/main/java/com/example/mycourses/chapters.java
+++ app/src/main/java/com/example/mycourses/chapters.java
@@ -2,6 +2,7 @@ package com.example.mycourses;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
--- app/src/main/java/com/example/mycourses/videoActivity.java
+++ app/src/main/java/com/example/mycourses/videoActivity.java
@@ -2,17 +2,44 @@ package com.example.mycourses;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.MediaController;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.VideoView;
 
 
 public class videoActivity extends AppCompatActivity {
 
     String url;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
         url = getIntent().getStringExtra("VidUrl");
+        progressBar = findViewById(R.id.progressBar);
+       // TextView title = findViewById(R.id.videoTitle);
+       // TextView description = findViewById(R.id.videoDesc);
+        VideoView videoPlayer = findViewById(R.id.videoView);
+
+
+
+        Uri videoUrl = Uri.parse(url);
+        videoPlayer.setVideoURI(videoUrl);
+        MediaController mc = new MediaController(this);
+        videoPlayer.setMediaController(mc);
+
+        videoPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                videoPlayer.start();
+                progressBar.setVisibility(View.GONE);
+            }
+        });
     }
 }
