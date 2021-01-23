529642c4fe3e115e5606e49b16b5bad5816ce02d
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-01-23 21:06:19 +0530

    Added Profile Activity

--- app/src/main/java/com/example/mycourses/Profile_Activity.java
+++ app/src/main/java/com/example/mycourses/Profile_Activity.java
@@ -1,20 +1,26 @@
 package com.example.mycourses;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.mikhaellopez.circularimageview.CircularImageView;
 import com.squareup.picasso.Picasso;
 
 public class Profile_Activity extends AppCompatActivity {
 
-    ImageView image;
+    CircularImageView image;
     TextView name, email;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +28,10 @@ public class Profile_Activity extends AppCompatActivity {
         image = findViewById(R.id.image);
         name = findViewById(R.id.name);
         email = findViewById(R.id.email);
+        toolbar = findViewById(R.id.mainToolbar);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
         if(signInAccount != null){
@@ -29,5 +39,19 @@ public class Profile_Activity extends AppCompatActivity {
             name.setText(signInAccount.getDisplayName());
             email.setText(signInAccount.getEmail());
         }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
