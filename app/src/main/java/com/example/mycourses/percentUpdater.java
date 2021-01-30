6bd356539823be74ff97e64b7c5949723689f4f2
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-01-30 20:13:37 +0530

    Adding Enrolled RecyclerView

---
+++ app/src/main/java/com/example/mycourses/percentUpdater.java
@@ -0,0 +1,13 @@
+package com.example.mycourses;
+
+
+
+public class percentUpdater {
+    String userId, subject, chapter;
+    public percentUpdater(String userId, String subject, String chapter) {
+        this.userId = userId;
+        this.subject = subject;
+        this.chapter = chapter;
+    }
+
+}
