06e7086ee71714bc6b51d10e3ab008d56b6ae3ee
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-02-08 11:12:32 +0530

    Checking Internet in SplashScreen

--- app/src/main/java/com/example/mycourses/SplashScreen.java
+++ app/src/main/java/com/example/mycourses/SplashScreen.java
@@ -11,12 +11,20 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.provider.Settings;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SplashScreen extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private DatabaseReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        mAuth = FirebaseAuth.getInstance();
+        userRef = FirebaseDatabase.getInstance().getReference("users");
     }
 
     @Override
