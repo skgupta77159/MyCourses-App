548defeb0c014daa1c8996f44d70cf025953e29d
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-02-08 11:09:00 +0530

    Checking Internet in SplashScreen

--- app/src/main/java/com/example/mycourses/SplashScreen.java
+++ app/src/main/java/com/example/mycourses/SplashScreen.java
@@ -1,8 +1,15 @@
 package com.example.mycourses;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +17,43 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(!isConnected(this)){
+            showDialogBox();
+        }
+    }
+
+    private void showDialogBox() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SplashScreen.this);
+        builder.setMessage("Please Connect to the Internet to Proceed Further")
+                .setCancelable(false)
+                .setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                }).show();
+    }
+
+    private boolean isConnected(SplashScreen splashScreen) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) splashScreen.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobile = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if((wifi!=null && wifi.isConnected()) || (mobile!=null && mobile.isConnected())){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
