fe83eebd9ebaf5a60cebf689a83b1d3ee7b87b75
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-02-09 10:12:33 +0530

    Added Setting Activity

--- app/src/main/java/com/example/mycourses/Setting.java
+++ app/src/main/java/com/example/mycourses/Setting.java
@@ -1,14 +1,19 @@
 package com.example.mycourses;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class Setting extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        toolbar = findViewById(R.id.mainToolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
