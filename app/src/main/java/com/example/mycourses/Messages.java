cd40e4b8ae4b67cd8cbb500d89934f5bc430d69e
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-02-13 10:03:33 +0530

    RecyclerView Messages Activity

--- app/src/main/java/com/example/mycourses/Messages.java
+++ app/src/main/java/com/example/mycourses/Messages.java
@@ -1,20 +1,65 @@
 package com.example.mycourses;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.mycourses.adapter.MessageAdapter;
+import com.example.mycourses.adapter.SubAdapter;
+import com.example.mycourses.model.messageModel;
+import com.example.mycourses.model.sublist;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Messages extends AppCompatActivity {
     Toolbar toolbar;
+    FirebaseDatabase database;
+    DatabaseReference messageRef;
+    List<messageModel> MsgList = new ArrayList<>();
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
         toolbar = findViewById(R.id.mainToolbar);
+        recyclerView = findViewById(R.id.msgRecyclerView);
 
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        database = FirebaseDatabase.getInstance();
+        messageRef = database.getReference("Messages");
+
+        messageRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    for(DataSnapshot chpSnapshot: snapshot.getChildren()){
+                        messageModel MsgInfo = chpSnapshot.getValue(messageModel.class);
+                        MsgList.add(MsgInfo);
+                        MessageAdapter messageAdapter = new MessageAdapter(MsgList, Messages.this);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(Messages.this));
+                        messageAdapter.notifyDataSetChanged();
+                        recyclerView.setAdapter(messageAdapter);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mycourses/model/messageModel.java
+++ app/src/main/java/com/example/mycourses/model/messageModel.java
@@ -2,5 +2,38 @@ package com.example.mycourses.model;
 
 public class messageModel {
 
-    
+    String iconURl, msgTitle, message;
+
+    public messageModel() {
+    }
+
+    public messageModel(String iconURl, String msgTitle, String message) {
+        this.iconURl = iconURl;
+        this.msgTitle = msgTitle;
+        this.message = message;
+    }
+
+    public String getIconURl() {
+        return iconURl;
+    }
+
+    public void setIconURl(String iconURl) {
+        this.iconURl = iconURl;
+    }
+
+    public String getMsgTitle() {
+        return msgTitle;
+    }
+
+    public void setMsgTitle(String msgTitle) {
+        this.msgTitle = msgTitle;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
